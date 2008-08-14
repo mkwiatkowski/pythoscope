9a8132019a1738e73a44cae0d3a0e04342e134d5
Michal Kwiatkowski <constant.beta@gmail.com>
2008-08-14 16:06:52 -0700

    Created basic directory layout and added simple setup.py file.

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='pythoscope',
+    version="0.1",
+
+    author = 'Michal Kwiatkowski',
+    author_email = 'constant.beta@gmail.com',
+    description = 'unit tests generator for Python',
+    license = 'GPLv3',
+    url = 'http://pythoscope.org',
+
+    packages = ['pythoscope'],
+)
