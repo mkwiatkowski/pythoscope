f44ebd992634274fa1ccd3ec452a9b8fc3b60144
Michal Kwiatkowski <constant.beta@gmail.com>
2008-08-21 11:21:26 -0700

    Version up to 0.2.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pythoscope',
-    version="0.1",
+    version="0.2",
 
     author = 'Michal Kwiatkowski',
     author_email = 'constant.beta@gmail.com',
