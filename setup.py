fa27f03b0df0329471700ea1ca0b168340ab9d54
U-MIG\mk <mk@mig>
2008-08-21 21:28:27 -0700

    Version up to 0.2.1.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pythoscope',
-    version="0.2",
+    version="0.2.1",
 
     author = 'Michal Kwiatkowski',
     author_email = 'constant.beta@gmail.com',
