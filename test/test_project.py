97cbf3e03eaf566882961fee4877addc17e4ab88
Michal Kwiatkowski <constant.beta@gmail.com>
2008-09-04 14:40:34 -0700

    Moved test_store.TestProject into test_project where it belongs.

--- test/test_project.py
+++ test/test_project.py
@@ -1,6 +1,8 @@
-from nose.tools import assert_equal
+from fixture import TempIO
+from nose.tools import assert_equal, assert_raises
 
-from pythoscope.store import Project, Module, TestModule, TestClass, TestMethod
+from pythoscope.store import Project, Module, Class, Function, TestModule, \
+     TestClass, TestMethod, ModuleNotFound
 
 from helper import assert_length, assert_equal_sets
 
@@ -10,17 +12,60 @@ TestClass.__test__ = False
 TestMethod.__test__ = False
 
 class TestProject:
-    def setUp(self):
-        self._old_test_module_save = TestModule.save
-        TestModule.save = lambda self: None
+    def test_can_be_saved_and_restored_from_file(self):
+        tmpdir = TempIO()
+        tmpdir.mkdir(".pythoscope")
+        modules = [Module(path='good_module.py',
+                          objects=[Class("AClass", ["amethod"]),
+                                   Function("afunction")]),
+                   Module(path='bad_module.py', errors=["Syntax error"])]
 
+        project = Project(tmpdir, modules)
+        project.save()
+        project = Project.from_directory(tmpdir)
+
+        assert_equal(2, len(project.modules))
+        assert_equal(2, len(project['good_module'].objects))
+        assert_equal("AClass", project['good_module'].classes[0].name)
+        assert_equal(["amethod"], project['good_module'].classes[0].methods)
+        assert_equal("afunction", project['good_module'].functions[0].name)
+        assert_equal(["Syntax error"], project['bad_module'].errors)
+
+    def test_can_be_queried_for_modules_by_their_path(self):
+        paths = ["module.py", "sub/dir/module.py", "package/__init__.py"]
+        project = Project(modules=map(Module, paths))
+
+        for path in paths:
+            assert_equal(path, project[path].path)
+
+    def test_raises_module_not_found_exception_when_no_module_like_that_is_present(self):
+        project = Project()
+        assert_raises(ModuleNotFound, lambda: project["whatever"])
+
+    def test_can_be_queried_for_modules_by_their_locator(self):
+        paths = ["module.py", "sub/dir/module.py", "package/__init__.py"]
+        locators = ["module", "sub.dir.module", "package"]
+        project = Project(modules=map(Module, paths))
+
+        for path, locator in zip(paths, locators):
+            assert_equal(path, project[locator].path)
+
+    def test_replaces_old_module_objects_with_new_ones_during_add_modules(self):
+        modules = map(Module, ["module.py", "sub/dir/module.py", "other/module.py"])
+        new_module = Module("other/module.py")
+
+        project = Project(modules=modules)
+        project.add_modules([new_module])
+
+        assert_length(project.modules, 3)
+        assert project["other/module.py"] is new_module
+
+class TestProjectWithTestModule:
+    def setUp(self):
         self.existing_test_class = TestClass("TestSomething")
         self.test_module = TestModule()
         self.test_module.add_test_case(self.existing_test_class)
         self.project = Project(modules=[self.test_module])
-
-    def tearDown(self):
-        TestModule.save = self._old_test_module_save
 
     def test_attaches_test_class_to_test_module_with_most_test_cases_for_associated_module(self):
         module = Module()
