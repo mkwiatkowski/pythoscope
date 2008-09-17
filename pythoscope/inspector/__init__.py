0a3a9d76ad7e750d56804cb815cc32ed52c0a529
Michal Kwiatkowski <constant.beta@gmail.com>
2008-09-17 16:11:48 -0700

    Don't run points of entry if the source code wasn't modified.

--- pythoscope/inspector/__init__.py
+++ pythoscope/inspector/__init__.py
@@ -4,6 +4,15 @@ from pythoscope.util import python_modules_below
 
 
 def inspect_project(project):
+    # If nothing new was discovered statically, it's also no use to run
+    # dynamic inspection.
+    if inspect_project_statically(project) > 0:
+        inspect_project_dynamically(project)
+    else:
+        print "Info: No changes discovered in the source code, skipping dynamic inspection."
+
+def inspect_project_statically(project):
+    count = 0
     for modpath in python_modules_below(project.path):
         try:
             module = project.find_module_by_full_path(modpath)
@@ -13,7 +22,10 @@ def inspect_project(project):
         except ModuleNotFound:
             pass
         static.inspect_module(project, modpath)
+        count += 1
+    return count
 
+def inspect_project_dynamically(project):
     for poe in project.points_of_entry.values():
         try:
             dynamic.inspect_point_of_entry(poe)
