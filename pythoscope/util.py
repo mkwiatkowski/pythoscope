3c480eee8002db274e9fed815154a3823ee97dde
Michal Kwiatkowski <constant.beta@gmail.com>
2009-02-12 23:20:58 +0100

    Ignore VCS files during inspection.

--- pythoscope/util.py
+++ pythoscope/util.py
@@ -179,9 +179,12 @@ def max_by_not_zero(func, collection):
         return None
 
 def python_modules_below(path):
+    VCS_PATHS = set([".bzr", "CVS", "_darcs", ".git", ".hg", ".svn"])
     def is_python_module(path):
         return path.endswith(".py")
-    return filter(is_python_module, rlistdir(path))
+    def not_vcs_file(path):
+        return not set(path.split(os.path.sep)).intersection(VCS_PATHS)
+    return filter(not_vcs_file, filter(is_python_module, rlistdir(path)))
 
 def rlistdir(path):
     """Resursive directory listing. Yield all files below given path,
