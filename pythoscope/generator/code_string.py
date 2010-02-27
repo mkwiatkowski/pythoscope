01dc597c0bf9fb419ea699edcea4001da4d8a8fb
Michal Kwiatkowski <constant.beta@gmail.com>
2010-02-27 17:40:58 +0100

    Fixed CodeString documentation.

--- pythoscope/generator/code_string.py
+++ pythoscope/generator/code_string.py
@@ -4,12 +4,12 @@ from pythoscope.util import union
 
 class CodeString(str):
     """A string that holds information on the piece of code (like a function
-    or method call it) it represents.
+    or method call) it represents.
 
     `uncomplete` attribute denotes whether it is a complete, runnable code
     or just a template.
 
-    `imports` is a list of imports that this piece of code requires.
+    `imports` is a set of imports that this piece of code requires.
     """
     def __new__(cls, string, uncomplete=False, imports=None):
         if imports is None:
